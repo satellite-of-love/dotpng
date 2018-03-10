a526439a2999315645e04378aa05545fe782c459
Jessica Kerr <jessitron@gmail.com>
2018-03-10 20:28:50 +0000

    It is getting hte sha

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const { spawn } = require('child_process');
+var sha1 = require('./sha');
 
 // curl http://localhost:3000/dot/png -X POST -H "Content-Type: text/plain" -d "digraph Post { la -> ha }"
 
 /* GET users listing. */
 router.post('/png', function (req, res, next) {
+  const dot = req.body;
   console.log("Body is: " + req.body);
-  res.send('Data is: ' + JSON.stringify(req.body) + "\n");
+
+  if (!req.body) {
+    throw new Error("No body seen on request. Set content type to text/plain and send DOT")
+  }
+  const shaOfData = sha1(dot);
+
+  const cp = spawn("pwd");
+  cp.stdout.on("data", function(data) { console.log("stdout: " + data)});
+  cp.on("exit", function(code, signal) {
+      res.send('Code is: ' + code + ' sha is: ' + shaOfData + "\n");
+  });
 });
 
 module.exports = router;
