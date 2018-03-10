d4f4dd3bf916b1c6153cdbae78b81c90d8e248f7
Jessica Kerr <jessitron@gmail.com>
2018-03-10 20:37:44 +0000

    this might even work

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { spawn } = require('child_process');
 var sha1 = require('./sha');
+var fs = require('fs');
 
 // curl http://localhost:3000/dot/png -X POST -H "Content-Type: text/plain" -d "digraph Post { la -> ha }"
 
@@ -15,10 +16,18 @@ router.post('/png', function (req, res, next) {
   }
   const shaOfData = sha1(dot);
 
-  const cp = spawn("pwd");
+  const filename = "public/images/" + shaOfData + ".png";
+
+  if (fs.existsSync(filename)) {
+    return res.redirect(filename);
+  }
+
+  const cp = spawn("dot", ["-Tpng", "-o", filename]);
+  cp.stdin.write(dot);
+  cp.stdin.end();
   cp.stdout.on("data", function(data) { console.log("stdout: " + data)});
   cp.on("exit", function(code, signal) {
-      res.send('Code is: ' + code + ' sha is: ' + shaOfData + "\n");
+      res.redirect(filename);
   });
 });
 
