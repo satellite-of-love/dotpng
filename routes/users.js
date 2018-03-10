3e7c962b393a69c9f64c281959c31f5e53660882
Jessica Kerr <jessitron@gmail.com>
2018-03-10 20:05:06 +0000

    tthis also works

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+const { spawn } = require('child_process');
 
 // curl http://localhost:3000/dot/png -X POST -H "Content-Type: text/plain" -d "digraph Post { la -> ha }"
-
 
 /* GET users listing. */
 router.post('/png', function (req, res, next) {
