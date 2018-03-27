ed941b6a3341aa31a22777a45b9becef4e0f1de5
Jessica Kerr <jessitron@gmail.com>
2018-03-27 11:06:04 -0500

    default service deployment compatibility
    listen on 8080
    implement /health and /info

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* healthcheck. */
+router.get('/health', function (req, res, next) {
+  res.send(`{ "status": "I'm OK" }`);
+});
+
+/* more healthcheck. */
+router.get('/info', function (req, res, next) {
+  res.send(`{ "whoami": "My job is to run graphviz, and give you a URL to an image. post DOT as text/plain to /dot/png" }`);
 });
 
 module.exports = router;
